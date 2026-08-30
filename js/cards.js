02b1ce6498b56f4d762560cc217ba54a5d37cabf
jackyttyy91-lang <jackyttyy91@gmail.com>
2026-08-31 00:24:04 +0800

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -9,44 +9,44 @@ const CARDS = {
     ],
     d2: [
         "Side Increaser",
-        x=>`Increase your maximum number of side by <b class='green'>5</b>`,
-        x=>x=="player",
-        x=>{
-            data[x].max_s += 5
-        },
-    ],
-    d3: [
-        "Minimum Side Increaser",
-        x=>`Increase ${['your',"enemy's"][x]} minimum number of side by <b class='green'>2</b>`,
-        x=>data[x].min_s<data[x].max_s && data.round < 10,
-        x=>{
-            data[x].min_s += 2
-        },
-    ],
-    d4: [
-        "Side Translation",
-        x=>`Increase ${['your',"enemy's"][x]} minimum & maximum number of side by <b class='green'>1</b>`,
-        x=>true,
-        x=>{
-            data[x].min_s += 1
-            data[x].max_s += 1
-        },
-    ],
-    d5: [
-        "Side Re-Increaser",
         x=>`Increase your maximum number of side by <b class='green'>10</b>`,
         x=>x=="player",
         x=>{
             data[x].max_s += 10
         },
     ],
-    d6: [
-        "Side Expansion",
+    d3: [
+        "Minimum Side Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} minimum number of side by <b class='green'>2</b>`,
+        x=>data[x].min_s<data[x].max_s,
+        x=>{
+            data[x].min_s += 5
+        },
+    ],
+    d4: [
+        "Side Translation",
         x=>`Increase your minimum & maximum number of side by <b class='green'>5</b>`,
         x=>x=="player",
         x=>{
             data[x].min_s += 5
             data[x].max_s += 5
+        },
+    ],
+    d5: [
+        "Side Re-Increaser",
+        x=>`Increase your multiply maximum number of side by <b class='green'>2</b>`,
+        x=>x=="player",
+        x=>{
+            data[x].max_s *= 2
+        },
+    ],
+    d6: [
+        "Side Expansion",
+        x=>`Increase your multiply minimum & maximum number of side by <b class='green'>5</b>`,
+        x=>x=="player",
+        x=>{
+            data[x].min_s *= 2
+            data[x].max_s *= 2
         },
     ],
     d7: [
@@ -58,20 +58,20 @@ const CARDS = {
 
     s1: [
         "Enemy Sacrifice for Player Multiplier",
-        x=>`Sacrifice <b class='green'>80%</b> of enemy's starting health for your increasing the multiplier of product by <b class='green'>1</b>`,
+        x=>`Sacrifice <b class='green'>80%</b> of enemy's starting health for your increasing the multiplier of multiply by <b class='green'>1.5</b>`,
         x=>x=="player",
         x=>{
             data.enemy.maxHealth = Math.floor(data.enemy.maxHealth*0.2)
-            data.player.mult += 1
+            data.player.mult *= 1.5
         },
     ],
     s2: [
         "Enemy Sacrifice for Player Multiplier Expansion",
-        x=>`Sacrifice <b class='green'>95%</b> of enemy's starting health for your increasing the multiplier of product by <b class='green'>3</b>`,
+        x=>`Sacrifice <b class='green'>95%</b> of enemy's starting health for your increasing the multiplier of multiply by <b class='green'>2</b>`,
         x=>x=="player",
         x=>{
             data.enemy.maxHealth = Math.floor(data.enemy.maxHealth*0.05)
-            data.player.mult += 3
+            data.player.mult *= 2
         },
     ],
 
@@ -80,7 +80,7 @@ const CARDS = {
         x=>`Increase your maximum energy by <b class='green'>10</b>`,
         x=>x=="player",
         x=>{
-            data[x].maxEnergy += 10
+            data[x].maxEnergy *= 2
         },
     ],
     e2: [
@@ -88,7 +88,7 @@ const CARDS = {
         x=>`Increase your maximum energy by <b class='green'>20</b>`,
         x=>x=="player",
         x=>{
-            data[x].maxEnergy += 20
+            data[x].maxEnergy *= 3
         },
     ],
     e3: [
@@ -102,7 +102,7 @@ const CARDS = {
         x=>`Increase ${['your',"enemy's"][x]} maximum energy by <b class='green'>5</b>`,
         x=>true,
         x=>{
-            data[x].maxEnergy += 5
+            data[x].maxEnergy += 1
         },
     ],
     
@@ -116,26 +116,26 @@ const CARDS = {
     ],
     en2: [
         "Stronger Multiplier Increaser",
-        x=>`Increase your multiplier by <b class='green'>1</b>`,
+        x=>`Increase your multiplier multiply by <b class='green'>2</b>`,
         x=>x=="player",
         x=>{
-            data[x].mult += 1
+            data[x].mult *= 2
         },
     ],
     en3: [
         "Mega Multiplier Increaser",
-        x=>`Increase your multiplier by <b class='green'>2</b>`,
+        x=>`Increase your multiplier multiply by <b class='green'>4</b>`,
         x=>x=="player",
         x=>{
-            data[x].mult += 2
+            data[x].mult *= 4
         },
     ],
     en4: [
         "Catastrophic",
-        x=>`Increase your multiplier by <b class='green'>4</b>`,
+        x=>`Increase your multiplier multiply by <b class='green'>8</b>`,
         x=>x=="player",
         x=>{
-            data[x].mult += 4
+            data[x].mult *= 8
         },
     ],
     en5: [
@@ -157,10 +157,10 @@ const CARDS = {
     ],
     m2: [
         "Multiplier Expansion",
-        x=>`Increase your multiplier by <b class='green'>0.75</b>`,
+        x=>`Increase your multiplier multiply by <b class='green'>1.75</b>`,
         x=>x=="player",
         x=>{
-            data[x].mult += 0.75
+            data[x].mult *= 1.75
         },
     ],
 
@@ -182,7 +182,7 @@ const CARDS = {
     o3: [
         "Multi, Max Energy, Side Translation and Divide Enemy Starting Health",
         x=>`Multiply your multi, max energy and minimum & maximum number of side by 4, Divide enemy's multi, max energy, starting health and minimum & maximum number of side by 4`,
-        x=>x=="player"&&Math.random()<1/5,
+        x=>x=="player",
         x=>{
             data.player.min_s *= 4
             data.player.max_s *= 4
